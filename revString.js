846451186e0a360bf59e16e62ef61fb2bb7b6a59
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-06-10 10:57:48 +0200

    add a function that reverses a string

--- revString.js
+++ revString.js
@@ -2,20 +2,19 @@
 // Write a function that takes a string as a parameter and returns the reverse of that string.
 
 function revString(name) {
-    return name.split("").reverse().join("");
+  return name.split("").reverse().join("");
 }
 
-console.log(revString("hello"))
-
+console.log(revString("hello"));
 
 // without using built-in functions
 
 function revString(name) {
-    newName = "";
-    for (let i = name.length - 1; i>=0; i--) {
-        newName += name[i];
-    }
-    return newName;
+  newName = "";
+  for (let i = name.length - 1; i >= 0; i--) {
+    newName += name[i];
+  }
+  return newName;
 }
 
-console.log(revString("hello"))
+console.log(revString("hello"));
