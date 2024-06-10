fe59357f9d93ff75ed16a5e25093eac6f50555ef
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-06-10 10:45:51 +0200

    add a function that calculates the area of a rectangle

---
+++ area.js
@@ -0,0 +1,8 @@
+// 1. Calculate the Area of a Rectangle:
+// Write a function that takes the length and width of a rectangle as parameters and returns the area of the rectangle.
+
+function area(length, width) {
+    return width * length
+}
+
+console.log(area(3, 7))
